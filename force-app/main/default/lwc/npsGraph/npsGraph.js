fed556c4ee5a3c180fb408828af7795d79eb56d2
Sunny Mopada <smopada@salesforce.com>
2024-09-05 11:13:59 +0530

    add onclick for reply suggestions to get latest data

--- force-app/main/default/lwc/npsGraph/npsGraph.js
+++ force-app/main/default/lwc/npsGraph/npsGraph.js
@@ -1,7 +1,7 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
 export default class NpsGraph extends LightningElement {
-    @api pointerValue = 75; // Configurable value to point at
+    @track pointerValue = 50; // Configurable value to point at
 
     get barStyle() {
         return 'background: linear-gradient(to right, green, yellow, orange, red); height: 20px; width: 100%;';
